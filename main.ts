d9d7e4a6dd8f6107c93a7cf2658858cdbae9d819
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2023-03-06 12:44:44 +0600

    Go again

--- main.ts
+++ main.ts
@@ -3,13 +3,9 @@ import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 const app = new Application();
 const router = new Router();
 
-router.get("/google", (ctx) => {
-    ctx.response.body = "md -Path $env:temp\firefoxinstall -erroraction SilentlyContinue | Out-Null $Download = join-path $env:temp\firefoxinstall firefox_installer.exe";
-    console.log("Google");
-});
-
 router.get("/", (ctx) => {
-    ctx.response.body = "Invoke-WebRequest 'https://download.mozilla.org/?product=firefox-latest&os=win64&lang=en-US' -OutFile $Download";
+    ctx.response.body = "Hello World!";
+    console.log("Request received");
 });
 
 app.use(router.routes());
