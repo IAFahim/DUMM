6712db3631b5b92c159bfc85eba2ec6578305d57
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2023-03-06 12:38:58 +0600

    Default

--- main.ts
+++ main.ts
@@ -1,18 +1,19 @@
 import {Application, Router} from "https://deno.land/x/oak/mod.ts";
-const URL= Deno.env.get("apikey");
-const JWT= Deno.env.get("Authorization");
 
 const app = new Application();
 const router = new Router();
 
 router.get("/GET", (context) => {
-    const queryParams = context.request.url.searchParams;
-
+    context.response.body = "Hello World!";
 })
 
 router.post("/POST", async (context) => {
     const body = await context.request.body();
     console.log(body.value);
+})
+
+router.put("/", async (context) => {
+    context.response.body = "Hello World!";
 })
 
 app.use(router.routes());
