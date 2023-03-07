17ce2cfb1cf6f8814417d38a28806ced03f295c8
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2023-03-07 13:47:13 +0600

    why post and get fire at same time

--- main.ts
+++ main.ts
@@ -6,14 +6,16 @@ const Api_key: string | undefined = Deno.env.get("Apikey");
 
 
 const router = new Router();
-router
-    .get("/", async (context) => {
-        const body = await context.request.body().value;
-        console.log(body + "get");
-    });
+router.get("/", async (context) => {
+    const body = await context.request.body().value;
+    console.log(body + "get");
+    context.response.status = 200;
+});
+
 router.post("/", async (context) => {
     const body = await context.request.body().value;
     console.log(body + "post");
+    context.response.status = 200;
 });
 
 const app = new Application();
