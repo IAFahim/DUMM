322684b78b22c634842ce0174fb8e14ca58a0a2a
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2023-03-07 12:15:13 +0600

    get post

--- main.ts
+++ main.ts
@@ -3,12 +3,14 @@ import {Application, Router} from "https://deno.land/x/oak@v11.1.0/mod.ts";
 const router = new Router();
 router
     .get("/", (context) => {
-      context.response.body = "Hello world!";
-    })
-
+        context.response.status = 200;
+    }).post("/", (context) => {
+    console.log(context.request.body)
+    context.response.status = 200;
+})
 
 const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-await app.listen({ port: 8000 });
+await app.listen({port: 80});
