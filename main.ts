884f3e8569a5089ac0a17993435548f7487c466c
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2023-03-07 14:46:36 +0600

    GO go

--- main.ts
+++ main.ts
@@ -10,18 +10,17 @@ const router = new Router();
 router.post("/", async (context) => {
     const body = await context.request.body().value;
     console.log(body);
+    const response = await fetch(`${url}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "apikey": `${Api_key}`,
+            "Authorization": `Bearer ${access_token}`,
+        },
+        body: body,
+    });
+    const json = await response.json();
     context.response.status = 200;
-    // const response = await fetch(`${url}`, {
-    //     method: "POST",
-    //     headers: {
-    //     "Content-Type": "application/json",
-    //     "apikey": `${Api_key}`,
-    //     "Authorization": `Bearer ${access_token}`,
-    //     },
-    //     body: JSON.stringify(data),
-    // });
-    // const json = await response.json();
-    // context.response.body = json;
 });
 
 router.get("/", async (context) => {
